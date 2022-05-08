34e98ab02aa4aa6346ae2972b390498413239b3e
LazerTony20 <72098802+LazerTony20@users.noreply.github.com>
2022-05-08 10:47:32 -0400

    creation fichier 3_8

---
+++ pb_APP_log_comb_E2022/pb_APP_log_comb.srcs/sources_1/new/Decodeur3_8.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/08/2022 010:27:03 AM
+-- Design Name: 
+-- Module Name: Decodeur3_8 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decodeur3_8 is
+    Port (	A2_3 : STD_LOGIC_VECTOR(2 downto 0),
+			LED	 : STD_LOGIC_VECTOR(7 downto 0);
+	);
+end decodeur3_8;
+
+
+
+
+architecture decodeur3_8_arch of decodeur3_8 is
+
+
+begin
+	with A2_3 select LED <=
+		"00000001" when "000",
+		"00000010" when "001",
+		"00000100" when "010",
+		"00001000" when "011",
+		"00010000" when "100",
+		"00100000" when "101",
+		"01000000" when "110",
+		"10000000" when "111",
+
+end decodeur3_8_arch;
