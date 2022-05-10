cfe8676d50878f8a15f69a8237b62bfd82d94c4e
LazerTony20 <72098802+LazerTony20@users.noreply.github.com>
2022-05-10 00:50:42 -0400

    Fixed compatibility of Add4bits and other modules

--- pb_APP_log_comb_E2022/pb_APP_log_comb.srcs/sources_1/new/Fct2_3.vhd
+++ pb_APP_log_comb_E2022/pb_APP_log_comb.srcs/sources_1/new/Fct2_3.vhd
@@ -33,17 +33,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Fct2_3 is
     Port ( ADCbin : in STD_LOGIC_VECTOR (3 downto 0);
-           A2_3 : out STD_LOGIC_VECTOR (2 downto 0));
+           A2_3 : out STD_LOGIC_VECTOR (3 downto 0));
 end Fct2_3;
 
 architecture Behavioral of Fct2_3 is
 
 component Add4bits is
-    Port ( X : in STD_LOGIC_vector(3 downto 0);
-           Y : in STD_LOGIC_vector(3 downto 0);
-           Ci : in STD_LOGIC;
-           S : out STD_LOGIC_vector(3 downto 0);
-           Co : out STD_LOGIC);
+    Port ( X4bit : in STD_LOGIC_vector(3 downto 0);
+           Y4bit : in STD_LOGIC_vector(3 downto 0);
+           Ci4bit : in STD_LOGIC;
+           S4bit : out STD_LOGIC_vector(3 downto 0);
+           Co4bit : out STD_LOGIC);
 end component;
 
 signal InputNumber : STD_LOGIC_VECTOR (3 downto 0);
@@ -58,16 +58,16 @@ begin
 --    InputNumber(3) <= '0';
 Inst_Fct2_3 : Add4bits
 Port map (
-    X(0) => ADCbin(1),
-    X(1) => ADCbin(2),
-    X(2) => ADCbin(3),
-    X(3) => '0',
-    Y(0) => ADCbin(3),
-    Y(1) => '0',
-    Y(2) => '0',
-    Y(3) => '0',
-    Ci => '0',
-    S => A2_3
+    X4bit(0) => ADCbin(1),
+    X4bit(1) => ADCbin(2),
+    X4bit(2) => ADCbin(3),
+    X4bit(3) => '0',
+    Y4bit(0) => ADCbin(3),
+    Y4bit(1) => '0',
+    Y4bit(2) => '0',
+    Y4bit(3) => '0',
+    Ci4bit => '0',
+    S4bit => A2_3
 );
 --    end process Inst_Fct2_3;
     
